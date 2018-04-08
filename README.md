51aad44b86264115bd93ca2e428a736584a2c00c
gfxjunkie <38183174+gfxjunkie@users.noreply.github.com>
2018-04-08 15:50:39 +0200

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# tumblrtest
-DLfavs
+tumblr-liked-photos-export
+=============
+
+Export the images from your [liked posts](https://www.tumblr.com/likes) in Tumblr.
+
+## How to use it
+
+1. Clone the repo:  
+
+        $ git clone git@github.com:javierarce/tumblr-liked-photos-export.git
+
+2. Enable the option "Share posts you like" in the settings page of your blog.
+ 
+3. Register a new app:  
+
+        https://www.tumblr.com/settings/apps
+
+4. Copy the `OAuth Consumer Key` of the app.  
+5. Add two keys to your bash/zsh:  
+
+        export TUMBLR_USERNAME="MY_FANTASTIC_TUMBLR_USERNAME"  
+        export TUMBLR_API_KEY="MY_DAUNTING_OAUTH_CONSUMER_KEY"  
+        
+        
+  Or simply update the `export.rb` file with your credentials:
+        
+        # Configuration
+        username = "MY_COOL_TUMBLR_USERNAME"
+        api_key  = "MY_OH_SO_NICE_OAUTH_CONSUMER_KEY"  
+
+6. Go to the app directory and run:  
+
+        ruby export.rb
+
+7. Your favorite images from Tumblr will be downloaded to the `image` folder.
+
+You can specify two params (`username` & `name` of the download directory) directly from the command line like this:
+
+         ruby export.rb username download_dir
+
+
+## Contributors
+
+[Javier Arce](https://github.com/javierarce)  
+[Sergi Meseguer](https://github.com/zigotica)
+
+## More
+
+Hey there, digital Diogenes, do you need to export more? Then check out the [tumblr-full-backup](https://github.com/zigotica/tumblr-full-backup) repo by [zigotica](https://github.com/zigotica).
---
+++ export.rb
@@ -0,0 +1,138 @@
+require 'rubygems'
+require 'httparty'
+
+# Configuration
+api_key      = ENV[vUZBorn6ZW8R3AWA5skryfZVzAbPVMVGKy8KLpmnya7FQrr5qL]
+username     = ARGV[0] || ENV[rsartpool]
+image_dir    = ARGV[1] || "images"
+offset       = 0
+limit        = 20  # number of posts requested each time
+download_num = 2500 # number of posts to download 
+
+class TumblrPhotoExport
+
+  attr_accessor :username, :api_key, :image_dir, :limit, :download_num, :url
+
+  def initialize(username, api_key, image_dir, limit, offset, download_num)
+
+    @username     = username
+    @api_key      = api_key
+    @image_dir    = image_dir
+    @limit        = limit
+    @offset       = offset
+    @download_num = download_num
+
+    @url          = "http://api.tumblr.com/v2/blog/#{@username}.tumblr.com/likes?api_key=#{@api_key}"
+
+    create_download_dir
+
+  end
+
+  def create_download_dir
+
+    Dir.mkdir("./#{@image_dir}") unless File.directory?("./#{@image_dir}")
+
+  end
+
+  def get_liked_count
+
+    response        = HTTParty.get(@url + "&limit=1&offset=0")
+    parsed_response = JSON.parse(response.body)
+
+    return parsed_response['response']['liked_count']
+
+  end
+
+  def get_photos(limit = 0, offset = 0)
+
+    response        = HTTParty.get(@url + "&limit=#{limit}&offset=#{offset}")
+    parsed_response = JSON.parse(response.body)
+
+    # Status of the request
+    status_code = parsed_response['meta']['status']
+    status_msg  = parsed_response['meta']['msg']
+
+    if status_code != 200
+      puts "\033[91m#{status_msg}\033[0m" 
+      return
+    end
+
+    download_likes(parsed_response['response']['liked_posts'])
+
+    return true
+
+  end
+
+  def download_likes(likes)
+
+    likes.each do |like|
+
+      photos = like['photos']
+
+      puts "\033[37m#{like['blog_name']}\033[0m" if photos and photos.length > 0
+
+      photos.each do |photo|
+
+        begin
+
+          uri = photo['original_size']['url']
+          file = File.basename(uri)
+
+          File.open("./#{@image_dir}/" + file, "wb") do |f| 
+            puts "   #{uri}"
+            f.write HTTParty.get(uri).parsed_response
+          end
+
+        rescue => e
+          puts ":( #{e}"
+        end
+
+      end if photos
+    end
+
+  end
+
+  def download
+
+    # uncomment next line to download all your liked images
+    # @download_num = get_liked_count
+
+    parsed = 0
+
+    rest = @download_num % @limit
+
+    if rest > 1
+      rest = 1
+    end
+
+    batchs = (@download_num / @limit) + rest
+
+    if (@download_num < @limit)
+      batchs = 1
+      @limit  = @download_num
+    end
+
+    puts "Downloading \033[32m#{@download_num}\033[0m posts"
+
+    batchs.times do |i|
+
+      offset = @offset + i*@limit
+
+      if parsed + @limit > @download_num
+        @limit = @download_num - parsed
+      end
+
+      result = get_photos(@limit, offset)
+      parsed += @limit
+      break if !result
+
+    end
+
+    puts "\033[32m#{"Aaaaand we're done, parsed #{parsed} "}\033[0m"
+
+  end
+
+end
+
+tumblr = TumblrPhotoExport.new(username, api_key, image_dir, limit, offset, download_num)
+tumblr.download
